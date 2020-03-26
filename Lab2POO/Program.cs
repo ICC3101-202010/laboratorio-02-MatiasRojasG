f9f7a00da13aec58ff7c756761a01c47e0a0a4b8
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-03-26 13:31:54 -0300

    Cancion lista, falta spotify

--- Lab2POO/Program.cs
+++ Lab2POO/Program.cs
@@ -6,8 +6,39 @@ namespace Lab2POO
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Buenos días Matias");
+            Console.WriteLine("Menú: Elija una opcion numerica");
+            Console.WriteLine("1) Ver Canciones");
+            Console.WriteLine(" ");
+            Console.WriteLine("2) Agregar Cancion");
+            Console.WriteLine(" ");
+            Console.WriteLine("3) Salir de Espotifai");
+            Console.WriteLine(" ");
+
+            string respuesta;
+            respuesta = Console.ReadLine();
+            if (respuesta == "1")
+            {
+                //Espotifai.VerCanciones()
+            }
+            if (respuesta == "2")
+            {
+                //Espotifai.AgregarCancion()
+            }
+            if (respuesta == "3")
+            {
+                Console.WriteLine("Chao qlo xd");
+            }
+            else
+            {
+                Console.WriteLine("Opcion no valida");
+            }
+
+
+
+
+
         }
     }
 }
-//kie
