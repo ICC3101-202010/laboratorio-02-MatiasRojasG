8ab23a8d9cd48bbfdadbac3be0f62fee40d0790a
MatiasRojasG <62114178+MatiasRojasG@users.noreply.github.com>
2020-03-29 22:48:57 -0300

    Arreglo nombre Espotifai

--- Lab2POO/Espotifai.cs
+++ Lab2POO/Espotifai.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
 namespace Lab2POO
 {
     public class Espotifai
     {
+        public List<Cancion> canciones = new List<Cancion> { };
+
         public Espotifai()
         {
+
         }
+        public bool AgregarCancion(Cancion cancion, List<Cancion> canciones)
+        {
+            if (canciones.Contains(cancion))
+            {
+                return false;
+            }
+            else
+            {
+                canciones.Add(cancion);
+                return true;
+            }
+
+        }
+        public void VerCanciones()
+        {
+            foreach (Cancion iden in canciones)
+            {
+                iden.Informacion();
+            }
+        }
+
     }
 }
